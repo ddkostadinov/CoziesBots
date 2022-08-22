0b9c5f6718df9d5fb8f2e78383c4517b1f20300e
david <david.kostadinov49@gmail.com>
2022-08-22 10:35:30 +0300

    Fixed seconds to hours

--- atmosphere/atmo.py
+++ atmosphere/atmo.py
@@ -20,7 +20,7 @@ async def on_ready():
   print("We have logged in as {0.user}".format(client))
   myLoop.start()
 
-@tasks.loop(seconds=20)
+@tasks.loop(hours=1)
 async def myLoop():
   channel = client.get_channel(989451180843352114)
   await channel.send(random.choice(weather.the_weather))
