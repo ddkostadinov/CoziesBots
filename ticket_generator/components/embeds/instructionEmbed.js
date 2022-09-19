1ae1af070033fe5c2e72c42034e4c52a2c29f0ce
david <david.kostadinov49@gmail.com>
2022-09-19 19:24:51 +0300

    removing URL from embed

--- ticket_generator/components/embeds/instructionEmbed.js
+++ ticket_generator/components/embeds/instructionEmbed.js
@@ -4,7 +4,7 @@ const blueBackground = new AttachmentBuilder('../assets/blue.png');
 const instructionEmbed = new EmbedBuilder()
 	.setColor(0x0099FF)
 	.setTitle('INSTRUCTION MANUAL')
-	.setURL('https://cozies.io')
+	
 	.setAuthor({ name: 'COZ-135 ID CARD GENERATOR', iconURL: 'https://i.imgur.com/cgprakZ.png', url: 'https://cozies.io' })
 	.setDescription('Follow the steps below to create your very own Cozy ID Card')
 	.setThumbnail('https://i.imgur.com/cgprakZ.png')
