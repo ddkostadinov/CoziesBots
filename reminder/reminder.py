0ae82f47079f27765c52ca6755d732e94ab88310
david <david.kostadinov49@gmail.com>
2022-10-10 00:57:39 +0300

    added candle

--- reminder/reminder.py
+++ reminder/reminder.py
@@ -16,7 +16,7 @@ intents.message_content = True
 my_secret = 'MTAyMjk1NzY3MDA4MjI5Mzg1MQ.GYGHKl.0qPVInn4Ccs9CdrN2ik6LrvS8bRTPf14loENFo' #change after test
 client = discord.Client(intents=intents)
 
-copies = ["Please remember to **TURN OFF DMs**. Our mods will **NEVER DM** you.  Our **ONLY OFFICIAL LINKS** are on <#1011295620297928825>. Stay safe, and **#StayCozy**"]
+copies = ["Please remember to **TURN OFF DMs**. Our mods will **NEVER DM** you.  Our **ONLY OFFICIAL LINKS** are on <#1011295620297928825>. Stay safe, and **#StayCozy** <:cozy_candle:1021415114861195337> "]
 
 
 
