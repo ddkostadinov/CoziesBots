adefe16d13287baf979efdcdf305889e1e247a99
david <david.kostadinov49@gmail.com>
2022-10-20 10:45:51 +0300

    reminder diamond

--- reminder/reminder.py
+++ reminder/reminder.py
@@ -16,7 +16,7 @@ intents.message_content = True
 my_secret = 'MTAyMjk1NzY3MDA4MjI5Mzg1MQ.GYGHKl.0qPVInn4Ccs9CdrN2ik6LrvS8bRTPf14loENFo' #change after test
 client = discord.Client(intents=intents)
 
-copies = ["Please remember to **TURN OFF DMs**. Our mods will **NEVER DM** you.  Our **ONLY OFFICIAL LINKS** are on <#1011295620297928825>. Stay safe, and **#StayCozy** <:cozy_candle:1021415114861195337> "]
+copies = ["The Diamond Ticket. Trade, reveal or keep it - it's your choice. More about the mint method here: <#1026854780934565958> / Learn how to get whitelisted <#1032246877367840818>"]
 
 
 
@@ -28,9 +28,9 @@ async def on_ready():
 
 @tasks.loop()
 async def myLoop():
-  if time_now().minute == 30 and time_now().second == 1:
+  if time_now().minute == 0 and time_now().second == 1:
       channel = client.get_channel(989451180843352114)  # change after test
-      await channel.send(random.choice(copies))
+      await channel.send(random.choice(copies), file=discord.File('./theDiamondTicket.mov'))
       
       time.sleep(2)
   
