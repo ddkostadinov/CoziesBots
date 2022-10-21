f8997119a624552381cf9507cf00b2840746104c
david <david.kostadinov49@gmail.com>
2022-10-21 14:50:15 +0300

    nomral ticket

--- normal_generator/generateImage.js
+++ normal_generator/generateImage.js
@@ -1,68 +1,69 @@
-const Canvas = require("canvas")
-const Discord = require("discord.js")
-const colorbg = require("./index")
+const Canvas = require("canvas");
+const Discord = require("discord.js");
+const colorbg = require("./index");
 
 //const background = "https://i.imgur.com/cgprakZ.png"
-let background = "bgto"
+let background = "bgto";
 
 const applyText = (canvas, text) => {
-	const context = canvas.getContext('2d');
-	let fontSize = 42;
+  const context = canvas.getContext("2d");
+  let fontSize = 40;
 
-	do {
-		context.font = `${fontSize -= 10}pt Futura MdCn BT`;
-	} while (context.measureText(text).width > canvas.width - 300);
+  do {
+    context.font = `${(fontSize -= 10)}pt Futura MdCn BT`;
+  } while (context.measureText(text).width > canvas.width - 300);
 
-    if (text.length >= 20) {
-        context.font = `${fontSize -= 3}pt Futura MdCn BT`;
-    }
-	return context.font;
+  if (text.length >= 20) {
+    context.font = `${(fontSize -= 3)}pt Futura MdCn BT`;
+  }
+  return context.font;
 };
 
 const dim = {
-    height: 1125,
-    width: 1500,
-    margin: 50
-}
+  height: 1125,
+  width: 1500,
+  margin: 50,
+};
 
 const av = {
-    size: 256,
-    x: 725,
-    y: 125
-}
+  size: 256,
+  x: 725,
+  y: 125,
+};
 
 const generateImage = async (member) => {
-    
-    let username = member.user.username
-    let discrim = member.user.discriminator
-    let name = colorbg.name_response
- 
-    const nametoUpper = name.toUpperCase()
+  let username = member.user.username;
+  let discrim = member.user.discriminator;
+  let name = colorbg.name_response;
 
-    //let avatarURL = member.user.displayAvatarURL({extension: "png", dynamic: false, size: av.size})
+  const nametoUpper = name.toUpperCase();
 
-    const canvas = Canvas.createCanvas(dim.width, dim.height)
-    const ctx = canvas.getContext("2d")
+  //let avatarURL = member.user.displayAvatarURL({extension: "png", dynamic: false, size: av.size})
 
-    //draw in the background
-    const backimg = await Canvas.loadImage(colorbg.bg)
-    ctx.drawImage(backimg, 0, 0)
+  const canvas = Canvas.createCanvas(dim.width, dim.height);
+  const ctx = canvas.getContext("2d");
 
-    // write in text
-    
-    ctx.fillStyle = "black" 
-    ctx.textAlign = "center"
-    
+  //draw in the background
+  const backimg = await Canvas.loadImage(colorbg.bg);
+  ctx.drawImage(backimg, 0, 0);
 
-    // draw in the username
-    
-    ctx.font = applyText(canvas, nametoUpper)
-    ctx.fillText(nametoUpper, dim.width - 1160, dim.height - dim.margin - 300 )
+  // write in text
 
+  ctx.shadowOffsetX = 0;
+  ctx.shadowColor = "#343C4B";
+  ctx.fillStyle = "#030508";
+  ctx.textAlign = "center";
 
-    const attachment = new Discord.AttachmentBuilder(canvas.toBuffer(), "welcome.png")
-    return attachment
+  // draw in the username
 
-}
+  ctx.font = applyText(canvas, nametoUpper);
+  ctx.fillText(nametoUpper, dim.width - 1145, dim.height - dim.margin - 315);
 
-module.exports = generateImage
+  const attachment = new Discord.AttachmentBuilder(
+    canvas.toBuffer(),
+    "welcome.png"
+  );
+  return attachment;
+};
+
+module.exports = generateImage;
--- normal_generator/index.js
+++ normal_generator/index.js
@@ -1,79 +1,66 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const Discord = require("discord.js")
-const generateImage = require("./generateImage")
+const { Client, GatewayIntentBits } = require("discord.js");
+const Discord = require("discord.js");
+const generateImage = require("./generateImage");
 
-require("dotenv").config()
+require("dotenv").config();
 
-const components = require("./components/components")
+const components = require("./components/components");
 
-const cardChannel = "1027224746552721499"
+const cardChannel = "982234712892379148";
 // const loggingChannel = "1021454453083226112"
 
 const client = new Discord.Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-	],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
 });
 
 let bg = "lolxd";
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
-
-
-
-client.on("messageCreate", async message => {
-    const cmd = message.content;
-    if(cmd == '!callnormal' && message.author.tag == 'djakozz#7269') {
-        await client.channels.cache.get(cardChannel).send({ content: "Print a ticket for your Cozy adventure.", components: [components.button]});
-    }
-})
-
-
-client.on('interactionCreate', async click => {
-    
-      
-    if(click.isButton()) {
-        if(click.customId == 'main_button') { 
-            let member = click.guild.members.cache.get(click.user.id);
-
-            bg = "https://i.imgur.com/KRtDiwS.png";
-            exports.bg = bg;
-
-            
-            await click.showModal(components.modal)
-        }
-  
-        
-    }
-
-    if (click.customId === "myModal") {
-        
-        const name_response = click.fields.getTextInputValue("nameInput");
-        
-        let member = click.guild.members.cache.get(click.user.id);
-
-        exports.name_response = name_response
-        
-
-        // generating banner
-        const img = await generateImage(member);
-        click.reply({
-        content: `*The machine silently vibrates and prints a ticket directly into your hand. It's still warm.*`, ephemeral: true, files: [img]
-        })
-
-        
-
-    }
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
+client.on("messageCreate", async (message) => {
+  const cmd = message.content;
+  if (cmd == "!callnormal" && message.author.tag == "djakozz#7269") {
+    await client.channels.cache.get(cardChannel).send({
+      content: "Print a ticket for your Cozy adventure.",
+      components: [components.button],
+    });
+  }
+});
 
+client.on("interactionCreate", async (click) => {
+  if (click.isButton()) {
+    if (click.customId == "main_button") {
+      let member = click.guild.members.cache.get(click.user.id);
 
-client.login(process.env.TOKEN)
+      bg = "https://i.imgur.com/fymG9Lf.png";
+      exports.bg = bg;
 
+      await click.showModal(components.modal);
+    }
+  }
 
+  if (click.customId === "myModal") {
+    const name_response = click.fields.getTextInputValue("nameInput");
 
+    let member = click.guild.members.cache.get(click.user.id);
+
+    exports.name_response = name_response;
+
+    // generating banner
+    const img = await generateImage(member);
+    click.reply({
+      content: `*The machine silently vibrates and prints a ticket directly into your hand. It's still warm.*`,
+      ephemeral: true,
+      files: [img],
+    });
+  }
+});
+
+client.login(process.env.TOKEN);
